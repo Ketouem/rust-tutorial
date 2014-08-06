50065de3efaa07d49d3a6de2418d44ff9b283243
Ketouem <ketouem@gmail.com>
2014-08-06 16:57:55 +0200

    Completed up to traits.

--- src/ch17-generics.rs
+++ src/ch17-generics.rs
@@ -14,3 +14,41 @@ fn map<T, U>(vector: &[T], function: |v: &T| -> U) -> Vec<U> {
 /*When defined with type parameters, as denoted by <T, U>, this function can be applied 
 to any type of vector, as long as the type of function's argument and the type of the 
 vector's contents agree with each other.*/
+
+/*Inside a generic function, the names of the type parameters (capitalized by convention) 
+stand for opaque types. All you can do with instances of these types is pass them around: 
+you can't apply any operations to them or pattern-match on them. Note that instances of 
+generic types are often passed by pointer.*/
+
+Generic type, struct, and enum declarations follow the same pattern:
+
+type Set<T> = std::collections::HashMap<T, ()>;
+
+struct Stack<T> {
+    elements: Vec<T>
+}
+
+enum Option<T> {
+    Some(T),
+    None
+}
+
+/*These declarations can be instantiated to valid types like Set<int>, Stack<int>, and Option<int>.*/
+
+/*The last type in that example, Option, appears frequently in Rust code. Because Rust does not have 
+null pointers (except in unsafe code), we need another way to write a function whose result isn't 
+defined on every possible combination of arguments of the appropriate types. The usual way is to write 
+a function that returns Option<T> instead of T.*/
+
+fn radius(shape: Shape) -> Option<f64> {
+    match shape {
+        Circle(_, radius) => Some(radius),
+        Rectangle(..)     => None
+    }
+}
+
+/*The Rust compiler compiles generic functions very efficiently by monomorphizing them. Monomorphization 
+is a fancy name for a simple idea: generate a separate copy of each generic function at each call site, 
+a copy that is specialized to the argument types and can thus be optimized specifically for them.*/
+
+// -- Traits
